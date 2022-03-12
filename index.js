8b89763f5754d87598d9b4aa6d2d606c95859122
Roland Leo Del Rosario <rleoadr@gmail.com>
2022-03-11 22:20:13 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function add ( num1 , num2) {
+    return 'num1 + num2';
+}
+function subtract ( num1 , num2) {
+    return 'num1 - num2';
+}
+function multiply ( num1 , num2) {
+    return 'num1 * num2';
+}
+function divide ( num1 , num2) {
+    return 'num1 / num2'
+}
+function add(a,b) {
+    let res = a + b
+    return res
+}
+function subtract(a,b) {
+    let res = a - b
+    return res
+}
+function multiply(a,b) {
+    let res = a * b
+    return res
+}
+function divide(a,b) {
+    let res = a / b
+    return res
+}
+function increment(n) {
+    return ++n;
+}
+function decrement(n) {
+    return --n;
+}
+function makeInt(n) {
+    var n = parseInt(n,10);
+    return n;
+}
+function preserveDecimal(n) {
+    var n = parseFloat(n,10);
+    return n;
+}
